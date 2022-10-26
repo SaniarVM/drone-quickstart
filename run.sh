4c858cce1f6e00a6a7d0d13d31166abf855b5e1b
Saniar EDUARDO <saniar.eduardo@aifrance.com>
2022-10-26 05:12:51 +0200

    new pipeline

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export DRONE_GITHUB_CLIENT_ID=
-export DRONE_GITHUB_CLIENT_SECRET=
-export DRONE_GITHUB_ADMIN=
-export DRONE_SERVER_HOST=
+export DRONE_GITHUB_CLIENT_ID=53c37d8c8a900970711a
+export DRONE_GITHUB_CLIENT_SECRET=a65e473a1bc8b9d9b05fc49e6edfd79884af129fb
+export DRONE_GITHUB_ADMIN=SaniarVM
+export DRONE_SERVER_HOST=25ca-80-15-34-36.eu.ngrok.io
 
 export HOSTNAME=$(hostname)
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
